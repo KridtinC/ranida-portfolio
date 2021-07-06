126cc2dc2ac8bad1cd454ce3a63fa0e42bfb3b72
KridtinC <kridtin.ch@gmail.com>
2021-07-07 03:33:28 +0700

    homepage done

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,20 @@
+import './Home.css'
+import featured1 from '../../assets/home/featured1.png'
+import featured2 from '../../assets/home/featured2.png'
+
 function Home() {
     return (
-        <div>Home Page</div>
+        <div className="home-container">
+            <div className="welcome-container">
+                <p className="welcome-title">HELLO!</p>
+                <p className="welcome-title">THIS IS <b className="welcome-title-name">RANIDA</b></p>
+                <p className="welcome-title-desc">and here is my portfolio!</p>
+            </div>
+            <div className="featured-container">
+                <img src={featured1} alt="" />
+                <img src={featured2} alt="" />
+            </div>
+        </div>
     )
 }
 
