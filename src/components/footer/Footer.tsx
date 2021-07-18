a11ed1442cbe66a6207e9a88da903900c9fa4ed4
KridtinC <kridtin.ch@gmail.com>
2021-07-18 20:43:47 +0700

    update comments

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,8 @@
+import './Footer.css'
+
 function Footer() {
     return (
-        <div style={{paddingBottom: "33px", paddingLeft: "80px", paddingRight: "80px", fontSize: '15px'}}><hr />© RANIDA PITH., 2021</div>
+        <div className="footer-text-container"><hr />© RANIDA PITH., 2021</div>
     )
 }
 
